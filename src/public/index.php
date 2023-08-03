6ae764d4c5bbff3ad4b7920b950994dafea7cae7
CMarchandon <camillemarchandon3@hotmail.fr>
2023-08-03 18:28:12 +0200

    1 exo

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,15 @@
-<?php
-declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Document</title>
+</head>
+<body>
+<h1> Link to the differents exercices </h1>
+<a href="./variable_types.php">variable_types.php</a>
 
-echo 'It works !';
+    
+</body>
+</html>
